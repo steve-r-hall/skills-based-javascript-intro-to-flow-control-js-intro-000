6c1d5f4814c52c6fdcceeaad01681089d493e0e8
Stephen Robert hall <steve.r.hall@outlook.com>
2019-08-18 08:37:03 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,7 +5,8 @@ function basicTeenager(age) {
 
 function teenager(age) {
   if (age >= 13 && age <= 19) {
-   return `You are a teenager!` }
+ return `You are a teenager!` }
+ else {return `You are not a teenager` }
 }
 
 function ageChecker(age) {
@@ -21,19 +22,19 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case 13 : return `You are a teenager!`;
+    case 13 : return `You are a teenager`;
             break;
-    case 14 : return `You are a teenager!`;
+    case 14 : return `You are a teenager`;
             break;
-    case 15:  return `You are a teenager!`;
+    case 15:  return `You are a teenager`;
             break;
-    case 16 : return `You are a teenager!`;
+    case 16 : return `You are a teenager`;
             break;
-    case 17 : return `You are a teenager!`;
+    case 17 : return `You are a teenager`;
             break;
-    case 18 : return `You are a teenager!`;
+    case 18 : return `You are a teenager`;
             break;
-    case 19 : return `You are a teenager!`;
+    case 19 : return `You are a teenager`;
             break;
     default:  return `You have an age`; }
 }
